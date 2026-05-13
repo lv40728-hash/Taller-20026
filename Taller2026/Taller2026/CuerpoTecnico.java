40e43769460641b2d897292b9f2e7d709d052909
Agustina-Porras <agussporras@gmail.com>
2026-05-13 15:31:20 -0300

    Refactor CuerpoTecnico to extend Persona class

--- Taller2026/Taller2026/CuerpoTecnico.java
+++ Taller2026/Taller2026/CuerpoTecnico.java
@@ -1,12 +1,14 @@
-public class CuerpoTecnico {
+public class CuerpoTecnico extends Persona {
     private Rol rol;
 
-    public CuerpoTecnico (Rol rol){
+    public CuerpoTecnico (Rol rol, String nombre, int fecNacimiento){
         this.rol = rol;
+        super(nombre, fecNacimiento);
     }
 
     public Rol setRol () {
         return rol;
+        super();
     }
     public void getRol (Rol rol) {
         this.rol = rol;
