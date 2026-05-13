2cf3cd27dd2de3ba0f27a016711a3dcccc2dc216
Agustina-Porras <agussporras@gmail.com>
2026-05-13 15:36:50 -0300

    Extend Arbitro class to inherit from Persona

--- Taller2026/Taller2026/Arbitro.java
+++ Taller2026/Taller2026/Arbitro.java
@@ -1,12 +1,16 @@
-public class Arbitro {
+import enum.CategoriaArbitro;
+
+public class Arbitro extends Persona {
     private int aniosExperiencia;
 
     public Arbitro () {
         this.aniosExperiencia = 0;
+        super();
     }
 
-    public Arbitro (int aniosExperiencia) {
+    public Arbitro (int aniosExperiencia, String nombre, int fecNacimiento) {
         this.aniosExperiencia = aniosExperiencia;
+        super(nombre, fecNacimiento);
     }
 
     public int setAniosExperiencia () {
