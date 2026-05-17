130b3246d46474e7a0554323f7a65c9023b8d880
Agustina-Porras <agussporras@gmail.com>
2026-05-17 19:48:05 -0300

    Add Sede attribute and related methods to Estadio

--- Taller2026/Taller2026/Estadio.java
+++ Taller2026/Taller2026/Estadio.java
@@ -1,15 +1,18 @@
 public class Estadio {
     private String nombre; 
     private int capacidad;
+    private Sede sede;
 
     public Estadio () {
         this.nombre = "";
         this.capacidad = 0; 
+        this.sede = sede;
     }
 
     public Estadio (String nombre,int capacidad) {
         this.nombre = nombre;
         this.capacidad = capacidad;
+        this.sede = sede;
     }
 
     public String setNombre () {
@@ -25,4 +28,13 @@ public class Estadio {
     public void getCapacidad (int capacidad) {
         this.capacidad = capacidad;
     }
+
+    public Sede getSede() {
+        return sede;
+    }
+
+    public void setSede(Sede sede) {
+        this.sede = sede;
+    }
+    
 }
