b24b2d276f87fca79cc809883c77543e36c9e08a
Agustina-Porras <agussporras@gmail.com>
2026-05-17 19:52:57 -0300

    Enhance Fase class with grupos and partidos management

--- Taller2026/Taller2026/Fase.java
+++ Taller2026/Taller2026/Fase.java
@@ -1,12 +1,20 @@
+import java.util.ArrayList;
+
 public class Fase {
     private String nombreFase;
+    private ArrayList<Grupo> grupos;
+    private ArrayList<Partido> partidos;
 
     public Fase () {
         this.nombreFase = "";
+        this.grupos = new ArrayList<>();
+        this.partidos = new ArrayList<>();
     }
 
     public Fase (String nombreFase) {
         this.nombreFase = nombreFase;
+        this.grupos = new ArrayList<>();
+        this.partidos = new ArrayList<>();
     }
 
     public String getNombreFase() {
@@ -17,5 +25,28 @@ public class Fase {
         this.nombreFase = nombreFase;
     }
 
+    public ArrayList<Grupo> getGrupos() {
+        return grupos;
+    }
+
+    public void setGrupos(ArrayList<Grupo> grupos) {
+        this.grupos = grupos;
+    }
+
+    public ArrayList<Partido> getPartidos() {
+        return partidos;
+    }
+
+    public void setPartidos(ArrayList<Partido> partidos) {
+        this.partidos = partidos;
+    }
+
+    public void agregarGrupo(Grupo g) {
+        this.grupos.add(g);
+    }
+
+    public void agregarPartido(Partido p) {
+        this.partidos.add(p);
+    }
     
 }
