58928d7ede2e815972851ba342b2a7b8d2df3982
Agustina-Porras <agussporras@gmail.com>
2026-05-17 18:58:12 -0300

    Enhance Pais class to manage Sede objects
    Added support for managing a list of 'Sede' objects in the 'Pais' class.

--- Taller2026/Taller2026/Pais.java
+++ Taller2026/Taller2026/Pais.java
@@ -1,15 +1,20 @@
+import java.util.ArrayList;
+
 public class Pais {
     private String nombre;
     private String bandera;
+    private ArrayList<Sede> sedes;
 
     public Pais () {
         this.nombre = "";
-        this.bandera = "";
+        this.bandera = ""; 
+        this.sedes = new ArratList<>();
     }
 
     public Pais ( String nombre, String bandera) {
         this.nombre = nombre;
         this.bandera = bandera;
+        this.sedes = new ArrayList<>();
     }
 
     public String setNombre ( ){
@@ -25,4 +30,18 @@ public class Pais {
     public void getBandera (String bandera) {
         this.bandera = bandera;
     }
+
+    public ArrayList<Sede> getSedes() {
+        return sedes;
+    }
+
+    public void setSedes(ArrayList<Sede> sedes) {
+        this.sedes = sedes;
+    }
+
+    public void agregarSede(Sede s) {
+        this.sedes.add(s);
+    }
+    
+    
 }
