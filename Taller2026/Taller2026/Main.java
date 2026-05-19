2f323cce4733fa0c24a6e8441768c4e6838c737d
Agustina-Porras <agussporras@gmail.com>
2026-05-19 15:26:53 -0300

    Implement main method to load Mundial infrastructure

--- Taller2026/Taller2026/Main.java
+++ Taller2026/Taller2026/Main.java
@@ -1,2 +1,37 @@
 public class Main{
-  public static void main(String[] args);
+  public static void main(String[] args){
+
+    Mundial mundial2026 = new Mundial();
+    
+    Pais mexico = new Pais("Mexico","Bandera de México");
+    Pais usa = new Pais("Estados Unidos", "Bandera de Estados Unidos");
+
+    Sede ciudaddemexico = new Sede("Ciudad de México", 2240.0f, "Templado", "UTC-6", mexico, mundial2026);
+    Sede nuevaYork = new Sede("Nueva York", 10.0f,"Humedo", "UTC-5", usa,mundial2026);
+
+    mexico.agregarSede(ciudaddemexico);
+    usa.agregarSede(nuevaYork);
+
+    Estadio azteca = new Estadio("Estadio Azteca", ciudaddemexico);
+    azteca.setCapacidad(87523);
+    ciudaddemexico.agregarEstadio(Azteca);
+
+    Estadio metLife = new Estadio("Estadio MetLife", nuevaYork);
+    metLife.setCapacidad(82500);
+    nuevaYork.agregarEstadio(metLife);
+
+    System.out.println(" Infraestructura del Mundial Cargada ");
+
+    System.out.println("\nPaís: " + mexico.getNombre() + " (" + mexico.getBandera() + ")");
+    System.out.println("Sede: " + cdmx.getCiudad() + " | Clima: " + cdmx.getClima() + " | Zona: " + cdmx.getZonaHoraria());
+    System.out.println(" -> Estadio: " + azteca.getNombre() + " (Capacidad: " + azteca.getCapacidad() + " espectadores)");
+
+    System.out.println("\nPaís: " + usa.getNombre() + " (" + usa.getBandera() + ")");
+    System.out.println("Sede: " + newYork.getCiudad() + " | Clima: " + newYork.getClima() + " | Zona: " + newYork.getZonaHoraria());
+    System.out.println(" -> Estadio: " + metLife.getNombre() + " (Capacidad: " + metLife.getCapacidad() + " espectadores)");
+
+  }
+}
+    
+
+  
